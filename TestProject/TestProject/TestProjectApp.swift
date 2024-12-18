d9a21925bcbfd8f7269ba7d4060857d29f80f7f4
Ani Ranjan Das <916857297@dhcp-lib-78-178.sfsu.edu>
2024-12-17 16:32:54 -0800

    test project

---
+++ TestProject/TestProject/TestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestProjectApp.swift
+//  TestProject
+//
+//  Created by Ani Ranjan Das on 12/17/24.
+//
+
+import SwiftUI
+
+@main
+struct TestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
